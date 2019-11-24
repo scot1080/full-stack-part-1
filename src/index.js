7bd56264291c2ead46c60e07c0c7f6e3c39214cc
Jordan <jordanscott1214@yahoo.com>
2019-11-24 13:20:52 -0500

    Exercise 1.10

--- src/index.js
+++ src/index.js
@@ -12,40 +12,7 @@ const Statistic = (props) => {
     <p>{props.text} {props.type}</p>
     )
 }
-const Statistics = (props) => {
-  const all = props.good + props.neutral + props.bad
-  let average = 0
-  if (all > 0){
-    average = props.good+(props.neutral*0)+(props.bad*(-1))/all
-  } else {
-     average = 0
-  }
-  let positive = 0
-  if (all > 0){
-      positive = props.good/all
-  } else {
-      positive = 0
-  }
-  if (all > 0){
-  return (
-  <div>
-    <h1>Statistics</h1>
-    <p>good {props.good}</p>
-    <p>neutral {props.neutral}</p>
-    <p>bad {props.bad}</p>
-    <p>all {all}</p>
-    <p>average {average}</p>
-    <p>positive {positive}</p>    
-  </div>
-)} else {
-      return (
-      <div>
-      <h1>Statistics</h1>
-      <p>No statistics have been collected</p>
-      </div>
-      )
-  }
-}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
