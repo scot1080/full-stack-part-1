0ee07cf8cb499ce502351fb36cad64dc2a5d361a
Jordan <jordanscott1214@yahoo.com>
2019-11-24 13:19:05 -0500

    Exercise 1.10

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = (props) => {
+    return(
+        <button onClick={()=>props.setType(props.type+1)}>{props.text}</button> 
+    )
+}
+
+const Statistic = (props) => {
+    return (
+    <p>{props.text} {props.type}</p>
+    )
+}
 const Statistics = (props) => {
   const all = props.good + props.neutral + props.bad
   let average = 0
@@ -40,15 +51,40 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-
+  const all = good + bad + neutral
+  let average = 0
+  if (all > 0){
+    average = good+(neutral*0)+(bad*(-1))/all
+  } else {
+     average = 0
+  }
+  let positive = 0
+  if (all > 0){
+      positive = good/all
+  } else {
+      positive = 0
+  }
+  if (all > 0){
   return (
     <div>
     <h1>Give Feedback</h1>
-    <button onClick={()=>setGood(good+1)}>Good</button><button onClick={()=>setNeutral(neutral+1)}>Neutral</button><button onClick={()=>setBad(bad+1)}>Bad</button>
-    <Statistics good={good} bad={bad} neutral={neutral}/>
+    <Button setType={setGood} type={good} text="Good"/><Button setType={setNeutral} type={neutral} text='Neutral'/><Button setType={setBad} type={bad} text="Bad"/>
+    <h1>Statistics</h1>
+    <Statistic type={all} text="All" />
+    <Statistic type={average} text="Average" />
+    <Statistic type={positive} text="Positive" />
     </div>
-  )
+    )
+    } else {
+    return (
+    <div>
+    <h1>Give Feedback</h1>
+    <Button setType={setGood} type={good} text="Good"/><Button setType={setNeutral} type={neutral} text='Neutral'/><Button setType={setBad} type={bad} text="Bad"/>
+    <h1>Statistics</h1>
+    <p>No statistics have been collected</p>
+    </div>
+      )
+  }
 }
 
 ReactDOM.render(<App />, 
