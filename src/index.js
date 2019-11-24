4921251c1793dd1ffced9159c46fcb53fcf95bef
Jordan <jordanscott1214@yahoo.com>
2019-11-24 13:52:42 -0500

    Exercise 1.12

--- src/index.js
+++ src/index.js
@@ -1,68 +1,27 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = (props) => {
-    return(
-        <button onClick={()=>props.setType(props.type+1)}>{props.text}</button> 
-    )
-}
+const App = (props) => {
+  const [selected, setSelected] = useState(Math.floor(Math.random() * 6))
 
-const Statistic = (props) => {
-    return (
-        <tr><td>{props.text}</td><td>{props.type}</td></tr>
-    )
-}
-
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const all = good + bad + neutral
-  let average = 0
-  if (all > 0){
-    average = (good+(neutral*0)+(bad*(-1)))/all
-  } else {
-     average = 0
-  }
-  let positive = 0
-  if (all > 0){
-      positive = ((good/all)*100).toFixed(2) + '%'
-  } else {
-      positive = 0
-  }
-  if (all > 0){
   return (
     <div>
-    <h1>Give Feedback</h1>
-    <Button setType={setGood} type={good} text="Good"/><Button setType={setNeutral} type={neutral} text='Neutral'/><Button setType={setBad} type={bad} text="Bad"/>
-    <table>
-        <thead>
-            <tr><th><h1>Statistics</h1></th></tr>
-        </thead>
-        <tbody>
-        <Statistic type={good} text="Good" />
-        <Statistic type={neutral} text="Neutral" />
-        <Statistic type={bad} text="Bad" />
-        <Statistic type={all} text="All" />
-        <Statistic type={average} text="Average" />
-        <Statistic type={positive} text="Positive" />
-        </tbody>
-    </table>
+      <p>{props.anecdotes[selected]}</p>
+      <button onClick={()=>setSelected(Math.floor(Math.random() * 6))}>next anecdote</button>
     </div>
-    )
-    } else {
-    return (
-    <div>
-    <h1>Give Feedback</h1>
-    <Button setType={setGood} type={good} text="Good"/><Button setType={setNeutral} type={neutral} text='Neutral'/><Button setType={setBad} type={bad} text="Bad"/>
-    <h1>Statistics</h1>
-    <p>No statistics have been collected</p>
-    </div>
-      )
-  }
+  )
 }
 
-ReactDOM.render(<App />, 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
